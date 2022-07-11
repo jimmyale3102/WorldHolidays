f8f4dd6175639d92c02301e7c9a725569507e2b5
unknown <jimmyale3102@gmail.com>
2022-07-11 16:24:34 -0500

    Add AutocompleteSearchBar

--- app/src/main/java/dev/alejo/world_holidays/MainActivity.kt
+++ app/src/main/java/dev/alejo/world_holidays/MainActivity.kt
@@ -1,22 +1,36 @@
 package dev.alejo.world_holidays
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import dev.alejo.world_holidays.ui.presentation.home.HomeViewModel
+import dev.alejo.world_holidays.ui.presentation.home.components.TextFieldWithDropdown
 import dev.alejo.world_holidays.ui.theme.WorldHolidaysTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    val all = listOf("aaa", "baa", "aab", "abb", "bab")
+
+    val dropDownOptions = mutableStateOf(listOf<String>())
+    val textFieldValue = mutableStateOf(TextFieldValue())
+    val dropDownExpanded = mutableStateOf(false)
+
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,11 +42,36 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val viewModel: HomeViewModel by viewModels()
                     viewModel.onCreate(this)
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ){
+                        Greeting("Android")
+
+                        TextFieldWithDropdown(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = textFieldValue.value,
+                            setValue = ::onValueChanged,
+                            onDismissRequest = ::onDropdownDismissRequest,
+                            dropDownExpanded = dropDownExpanded.value,
+                            list = dropDownOptions.value,
+                            label = "Label"
+                        )
+                    }
                 }
             }
         }
     }
+
+    private fun onDropdownDismissRequest() {
+        dropDownExpanded.value = false
+    }
+
+    private fun onValueChanged(value: TextFieldValue) {
+        dropDownExpanded.value = true
+        textFieldValue.value = value
+        dropDownOptions.value = all.filter { it.startsWith(value.text) && it != value.text }.take(3)
+    }
+
 }
 
 @Composable
---
+++ app/src/main/java/dev/alejo/world_holidays/ui/presentation/home/components/AutoCompleteSearchBar.kt
@@ -0,0 +1,105 @@
+package dev.alejo.world_holidays.ui.presentation.home.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
+import dev.alejo.world_holidays.ui.theme.LightWhite
+
+@Composable
+fun TextFieldWithDropdown(
+    modifier: Modifier = Modifier,
+    value: TextFieldValue,
+    setValue: (TextFieldValue) -> Unit,
+    onDismissRequest: () -> Unit,
+    dropDownExpanded: Boolean,
+    list: List<String>,
+    label: String = ""
+) {
+    val trailingIcon = @Composable {
+        IconButton(
+            onClick = { },
+        ) {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "",
+                tint = Color.White
+            )
+        }
+    }
+    Box(
+        modifier
+            .background(LightWhite)
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { focusState ->
+                    if (!focusState.isFocused)
+                        onDismissRequest()
+                }
+                .background(LightWhite),
+            value = value,
+            onValueChange = setValue,
+            label = { Text(text = label, color = Color.White) },
+            colors = TextFieldDefaults.textFieldColors(textColor = Color.White),
+            trailingIcon = trailingIcon,
+            shape = RoundedCornerShape(10.dp)
+        )
+        DropdownMenu(
+            expanded = dropDownExpanded,
+            properties = PopupProperties(
+                focusable = false,
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            ),
+            onDismissRequest = onDismissRequest
+        ) {
+            list.forEach { text ->
+                DropdownMenuItem(onClick = {
+                    setValue(
+                        TextFieldValue(
+                            text,
+                            TextRange(text.length)
+                        )
+                    )
+                }) {
+                    Text(text = text)
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Pre() {
+    val dropDownOptions = mutableStateOf(listOf<String>())
+    val textFieldValue = mutableStateOf(TextFieldValue())
+    val dropDownExpanded = mutableStateOf(false)
+
+    TextFieldWithDropdown(
+        modifier = Modifier.fillMaxWidth(),
+        value = textFieldValue.value,
+        setValue = { },
+        onDismissRequest = { },
+        dropDownExpanded = dropDownExpanded.value,
+        list = dropDownOptions.value,
+        label = "CHILE"
+    )
+}
