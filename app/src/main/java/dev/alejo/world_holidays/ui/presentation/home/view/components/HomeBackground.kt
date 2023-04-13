2a55d7afad9f6cb822b89a3d9f1e70bbd31e2196
jimmyale3102 <jimmyale3102@gmail.com>
2023-04-12 20:21:39 -0500

    Show background resources from local store

--- app/src/main/java/dev/alejo/world_holidays/ui/presentation/home/view/components/HomeBackground.kt
+++ app/src/main/java/dev/alejo/world_holidays/ui/presentation/home/view/components/HomeBackground.kt
@@ -1,5 +1,6 @@
 package dev.alejo.world_holidays.ui.presentation.home.view.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,10 +10,8 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import dev.alejo.world_holidays.core.Constants
+import androidx.compose.ui.res.painterResource
+import dev.alejo.world_holidays.R
 import dev.alejo.world_holidays.ui.theme.BlueLight
 
 @Composable
@@ -31,18 +30,40 @@ fun HomeBackground() {
                 )
             )
     ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(Constants.HOME_BACKGROUND_IMG_URL.format(getRandomNumber()))
-                .crossfade(true)
-                .build(),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
+        Image(
             modifier = Modifier
                 .fillMaxSize()
-                .alpha(0.2F)
+                .alpha(0.2F),
+            painter = painterResource(id = PainterBackground.getRandom()),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
         )
     }
 }
 
-private fun getRandomNumber() = (0..10).random()
+object PainterBackground {
+    private val backgrounds = arrayOf(
+        R.drawable.bg_0,
+        R.drawable.bg_1,
+        R.drawable.bg_2,
+        R.drawable.bg_3,
+        R.drawable.bg_4,
+        R.drawable.bg_5,
+        R.drawable.bg_6,
+        R.drawable.bg_7,
+        R.drawable.bg_8,
+        R.drawable.bg_9,
+        R.drawable.bg_10,
+        R.drawable.bg_11,
+        R.drawable.bg_12,
+        R.drawable.bg_13,
+        R.drawable.bg_14,
+        R.drawable.bg_15,
+        R.drawable.bg_16,
+        R.drawable.bg_17,
+        R.drawable.bg_18,
+        R.drawable.bg_19
+    )
+
+    fun getRandom() = backgrounds.random()
+}
