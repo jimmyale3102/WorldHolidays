fb7257a55eb6ac1004d70a0395b19e64d0e22449
jimmyale3102 <jimmyale3102@gmail.com>
2023-04-12 20:21:51 -0500

    Add splash to show it

--- app/src/main/java/dev/alejo/world_holidays/ui/presentation/splash/view/MainActivity.kt
+++ app/src/main/java/dev/alejo/world_holidays/ui/presentation/splash/view/MainActivity.kt
@@ -1,9 +1,10 @@
-package dev.alejo.world_holidays.ui
+package dev.alejo.world_holidays.ui.presentation.splash.view
 
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -12,19 +13,23 @@ import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.alejo.world_holidays.core.navigation.Navigation
 import dev.alejo.world_holidays.core.navigation.Screen
+import dev.alejo.world_holidays.ui.presentation.splash.viewmodel.SplashViewModel
 import dev.alejo.world_holidays.ui.theme.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: SplashViewModel by viewModels()
+
     @OptIn(ExperimentalAnimationApi::class)
     @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splashScreen = installSplashScreen()
+        installSplashScreen().setKeepOnScreenCondition {
+            viewModel.isLoading.value
+        }
         setContent {
             WorldHolidaysTheme {
-                splashScreen.setKeepOnScreenCondition { false }
                 Navigation(
                     navHostController = rememberAnimatedNavController(),
                     startDestination = Screen.Home.route
